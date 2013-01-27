d3c8d3b57680a6eda353a970448d1132f1385bf7
aacarioca <guto.carioca@gmail.com>
2013-01-27 18:46:43 +1100

    Update FlipDotDisplayController.h

--- FlipDotDisplayController.h
+++ FlipDotDisplayController.h
@@ -10,10 +10,9 @@
 
 class DotDisplay{
 public:
-  DotDisplay(int dataPin, int clockPin, int latchPin);
+	DotDisplay(int dataPin, int clockPin, int latchPin);
 	byte generateColumnCode (byte col);
 	void updateDisplay(char textMessage[]);
-	void updateDisplayArray(char textMessage[][16], int pause);
 	void setDot(byte col, byte row, bool on);
 	void disablePins();
 private:
